875abe416a2b30f22c01f0a3bd4335918d631491
isaiah ntina <isaiahntina47@gmail.com>
2022-07-06 18:55:52 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+void print_alphabet_x10(void)
+{
+	int test_case = 0;
+	char alpha;
+	while(test_case <10)
+	{
+		for(alpha='a'; alpha<='z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+		test_case++;
+		_putchar('\n');
+	}
+}
