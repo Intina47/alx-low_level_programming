0be0bf1cfe3b8a36dee3a06ee9387b1d5fa083d5
uzohou <uzohouzoma998@gmail.com>
2022-07-13 02:47:46 +0100

    pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+int _atoi(char *s)
+{
+    int i = 0;
+    int n = 0;
+    int sign = 1;
+    while (s[i] == ' ' || (s[i] >= '\t' && s[i] <= '\r'))
+    {
+        i++;
+    }
+    if (s[i] == '-')
+    {
+        sign = -1;
+        i++;
+    }
+    else if (s[i] == '+')
+    {
+        i++;
+    }
+    while (s[i] >= '0' && s[i] <= '9')
+    {
+        n = n * 10 + (s[i] - '0');
+        i++;
+    }
+    return (n * sign);
+}
