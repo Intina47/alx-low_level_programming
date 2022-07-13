310fdcafaf8770a57fd8a1e72b32bd66564cce9e
uzohou <uzohouzoma998@gmail.com>
2022-07-13 02:27:45 +0100

    pointers, arrays and strings

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,5 +1,10 @@
 #include "main.h"
 
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: array of integers
+ * @n: number of elements to print
+ */
 void print_array(int *a, int n)
 {
     int i;
