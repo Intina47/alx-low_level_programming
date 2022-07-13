46fa0bed069e4aee3b48ed25aab6f25d4c560b34
uzohou <uzohouzoma998@gmail.com>
2022-07-13 01:11:03 +0100

    pointers, arrays and strings

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -4,8 +4,9 @@ void rev_string(char *s)
 {
     int i;
 
-    for(i = _strlen(s)-1; i >= 0; i--)
+    for(i = i - 1; s[i] != '\0'; i--)
     {
         _putchar(s[i]);
     }
+    _putchar('\n');
 }
