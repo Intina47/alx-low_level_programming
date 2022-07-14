6c2f62e93a134eca4fad8e7b72e3e7b2b3c5d9a5
uzohou <uzohouzoma998@gmail.com>
2022-07-14 12:54:12 +0100

    part 2 pointer ...

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,23 +2,38 @@
 
 char *leet(char *)
 {
+   char *str = "";
     int i;
-    char *str;
-    str = malloc(sizeof(char) * 100);
-    if (str == NULL)
-        return (NULL);
     for (i = 0; str[i] != '\0'; i++)
     {
         if (str[i] == 'a' || str[i] == 'A')
+        {
             str[i] = '4';
+        }
         else if (str[i] == 'e' || str[i] == 'E')
+        {
             str[i] = '3';
-        else if (str[i] == 'l' || str[i] == 'L')
+        }
+        else if (str[i] == 'g' || str[i] == 'G')
+        {
+            str[i] = '6';
+        }
+        else if (str[i] == 'i' || str[i] == 'I')
+        {
             str[i] = '1';
+        }
         else if (str[i] == 'o' || str[i] == 'O')
+        {
             str[i] = '0';
+        }
+        else if (str[i] == 's' || str[i] == 'S')
+        {
+            str[i] = '5';
+        }
         else if (str[i] == 't' || str[i] == 'T')
+        {
             str[i] = '7';
+        }
     }
     return (str);
 }
